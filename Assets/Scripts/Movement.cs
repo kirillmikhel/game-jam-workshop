4596c9547318bee5aceaab3b10ac73d03147b949
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-01 21:27:00 +0200

    Extract PlayerInput out of Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,35 +16,8 @@ public class Movement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 direction)
     {
-        if (!_rigidbody) return;
-
-        Vector2 direction = Vector2.zero;
-
-        if (Input.GetAxisRaw("Vertical") == 1)
-        {
-            direction += Vector2.up * speed;
-        }
-
-        if (Input.GetAxisRaw("Vertical") == -1)
-        {
-            direction += Vector2.down * speed;
-        }
-
-        if (Input.GetAxisRaw("Horizontal") == 1)
-        {
-            direction += Vector2.right * speed;
-        }
-
-        if (Input.GetAxisRaw("Horizontal") == -1)
-        {
-            direction += Vector2.left * speed;
-        }
-
-
         _rigidbody.velocity = direction.normalized * speed;
 
         _animator.speed = direction == Vector2.zero ? 0 : 1;
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Movement _movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movement = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_movement) return;
+
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        _movement.Move(direction);
+    }
+}
