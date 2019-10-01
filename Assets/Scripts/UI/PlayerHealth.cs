b738c9caac1d39edf4f8465024e5fe6ad6d67062
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-01 23:00:59 +0200

    Add Player Health UI presentation

---
+++ Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+    private GameObject _player;
+    private Health _playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _playerHealth = _player.GetComponent<Health>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_rectTransform) return;
+        
+        _rectTransform.sizeDelta = new Vector2(_playerHealth.HP * 64, 64);
+    }
+}
