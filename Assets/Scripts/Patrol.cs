72b73e3e0102c705cfcdb25978bdb4ef0989f555
Kirill Mikhel <kirill.mikhel@gmail.com>
2019-10-01 22:41:47 +0200

    Add NPC patrolling

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public bool vertical = false;
+    
+    private Movement _movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movement = GetComponent<Movement>();
+
+        if (_movement) StartCoroutine(DoPatrol());
+    }
+
+    private IEnumerator DoPatrol()
+    {
+        Vector2 direction = vertical ? Vector2.up : Vector2.left;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+
+            _movement.Move(direction);
+
+            direction = -direction;
+        }
+    }
+}
