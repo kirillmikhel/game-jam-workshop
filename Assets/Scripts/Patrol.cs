97646827ba71ec1e7eea47e231718decbcc24026
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-03 21:14:50 +0100

    Make NPC patrolling all directions

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Patrol : MonoBehaviour
 {
     public bool vertical = false;
-    
+    public bool inverse = false;
+
     private Movement _movement;
 
     // Start is called before the first frame update
@@ -19,6 +20,11 @@ public class Patrol : MonoBehaviour
     private IEnumerator DoPatrol()
     {
         var direction = vertical ? Vector2.up : Vector2.left;
+
+        if (inverse)
+        {
+            direction = -direction;
+        }
 
         while (true)
         {
