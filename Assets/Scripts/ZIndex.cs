e83158bb732bc41c5a1a990af575d27faeb276cd
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-04 21:35:06 +0100

    Add Z index script for moving sprites

---
+++ Assets/Scripts/ZIndex.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZIndex : MonoBehaviour
+{
+    void Awake()
+    {
+        SetZPosition();
+    }
+
+    void Update()
+    {
+        SetZPosition();
+    }
+
+    private void SetZPosition()
+    {
+        var position = transform.position;
+
+        position.z = position.y;
+
+        transform.position = position;
+    }
+}
