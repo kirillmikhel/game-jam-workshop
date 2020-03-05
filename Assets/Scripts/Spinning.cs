faf98428dc412ee477816a77f53eaeb80a1ccb60
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-05 21:04:39 +0100

    Make stone spinning

---
+++ Assets/Scripts/Spinning.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinning : MonoBehaviour
+{
+    public float angularSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * angularSpeed);
+    }
+}
