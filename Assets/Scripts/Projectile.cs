a9b5853edf723ba5e0a294b65cbb89648ecfefc3
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-07 22:21:07 +0100

    Make stones destroyable and dealing damage

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public int damage = 1;
+    public List<string> tagsToIgnore;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (tagsToIgnore.Count > 0 && tagsToIgnore.Contains(other.tag)) return;
+
+        other.GetComponent<Health>()?.ReceiveDamage(damage);
+
+        Destroy(gameObject);
+    }
+}
