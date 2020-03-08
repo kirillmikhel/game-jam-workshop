9acb16721c37e9dd04f56776dfca92e5f581a231
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-08 21:28:38 +0100

    Add Stone hit particle effects

---
+++ Assets/Scripts/DestroyOnTimeout.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTimeout : MonoBehaviour
+{
+    public float timeout = 0.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke(nameof(Destroy), timeout);
+    }
+
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnOnDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnDestroy : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void OnDestroy()
+    {
+        var obj = Instantiate(prefab, transform.position, Quaternion.identity);
+
+        obj.transform.SetParent(null);
+    }
+}
