ad4007e3aeefe3e3e6d2b8ad0927e0cdbcd46dce
Kirill Mikhel <kirill.mikhel@gmail.com>
2020-03-09 23:22:03 +0100

    Add red highlighting as a damage indicator

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,6 +7,15 @@ public class Health : MonoBehaviour
     public int HP = 1;
     public int maxHP = 1;
 
+    private SpriteRenderer _spriteRenderer;
+    private Dropper _dropper;
+
+    private void Awake()
+    {
+        _dropper = GetComponent<Dropper>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void Increment()
     {
         HP = Mathf.Min(maxHP, HP + 1);
@@ -18,9 +27,29 @@ public class Health : MonoBehaviour
 
         GameManager.Instance.GetComponent<SoundController>().Play("Hit");
 
+        var coroutine = StartCoroutine(DisplayDamage());
+
         if (HP > 0) return;
 
-        GetComponent<Dropper>()?.Drop();
+        StopCoroutine(coroutine);
+        Invoke(nameof(Die), 0.05f);
+    }
+
+    private IEnumerator DisplayDamage()
+    {
+        var initialColor = _spriteRenderer.color;
+
+        _spriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.25f);
+
+        _spriteRenderer.color = initialColor;
+    }
+
+    private void Die()
+    {
+        if (_dropper) _dropper.Drop();
+
         Destroy(gameObject);
     }
 }
